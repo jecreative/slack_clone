b341724d10bd786b8229eba8b6f9282e15677b7e
Jordan Esguerra <creative@jordanesguerra.com>
2020-12-10 19:18:52 -0800

    3) Header component & styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import Header from './components/Header'
+
 function App() {
   return (
-    <div className='App'>
+    <div className='app'>
       <h1>Slack Clone</h1>
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import { Avatar } from '@material-ui/core'
+import AccessTimeIcon from '@material-ui/icons/AccessTime'
+import SearchIcon from '@material-ui/icons/Search'
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
+
+const Header = () => {
+  return (
+    <header>
+      <div className='header_left'>
+        <Avatar
+          className='header_avatar'
+          // alt={user?.displayName}
+          // src={user?.photoUrl}
+        />
+        <AccessTimeIcon />
+      </div>
+      <div className='header_search'>
+        <SearchIcon />
+        <input type='search' placeholder='Search' />
+      </div>
+      <div className='header_right'>
+        <HelpOutlineIcon />
+      </div>
+    </header>
+  )
+}
+
+export default Header
