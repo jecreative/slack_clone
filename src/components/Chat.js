1b19d80bfc63ec1cd22d96117a9f1e31fed3e1ed
Jordan Esguerra <creative@jordanesguerra.com>
2020-12-10 23:48:17 -0800

    10) Created chat component with header and styling

---
+++ src/components/Chat.js
@@ -0,0 +1,26 @@
+import { useParams } from 'react-router-dom'
+import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined'
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined'
+
+const Chat = () => {
+  const { roomId } = useParams()
+  return (
+    <div id='chat'>
+      <div className='chat_header'>
+        <div className='chat_headerLeft'>
+          <h4 className='chat_channelName'>
+            <strong># general</strong>
+            <StarBorderOutlinedIcon />
+          </h4>
+        </div>
+        <div className='chat_headerRight'>
+          <p>
+            <InfoOutlinedIcon /> Details
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Chat
