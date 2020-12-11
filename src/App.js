d5d41ddff16c9c5184c1b58ad244887ff688f5b2
Jordan Esguerra <creative@jordanesguerra.com>
2020-12-10 23:47:32 -0800

    9) Installed and setup react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
+import Chat from './components/Chat'
 
 function App() {
   return (
     <div className='app'>
-      <Header />
-      <main>
-        <Sidebar />
-      </main>
+      <Router>
+        <Header />
+        <main>
+          <Sidebar />
+          <Switch>
+            <Route path='/room/:roomId' component={Chat} />
+            <Route path='/'>
+              <h1>Welcome</h1>
+            </Route>
+          </Switch>
+        </main>
+      </Router>
     </div>
   )
 }
