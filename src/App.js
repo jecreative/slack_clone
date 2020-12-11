a66c2688b6fb068cd3bc44406cd7542530c97a15
Jordan Esguerra <creative@jordanesguerra.com>
2020-12-11 11:25:35 -0800

    18) Made login component protected based on user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
+import { useStateValue } from './context/StateProvider'
+
 import Header from './components/Header'
 import Sidebar from './components/Sidebar'
 import Chat from './components/Chat'
+import Login from './components/Login'
 
-function App() {
+const App = () => {
+  const [{ user }, dispatch] = useStateValue()
+
   return (
     <div className='app'>
       <Router>
-        <Header />
-        <main>
-          <Sidebar />
-          <Switch>
-            <Route path='/room/:roomId' component={Chat} />
-            <Route path='/'>
-              <h1>Welcome</h1>
-            </Route>
-          </Switch>
-        </main>
+        {!user ? (
+          <Login />
+        ) : (
+          <>
+            <Header />
+            <main>
+              <Sidebar />
+              <Switch>
+                <Route path='/room/:roomId' component={Chat} />
+                <Route path='/'>
+                  <h1>Welcome</h1>
+                </Route>
+              </Switch>
+            </main>
+          </>
+        )}
       </Router>
     </div>
   )
