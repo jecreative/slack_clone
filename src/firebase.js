34e5637e4e4d59e4e6bb298443d1292c7d1306af
Jordan Esguerra <creative@jordanesguerra.com>
2020-12-11 18:08:49 -0800

    34) Deployed project to firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,8 @@
-import firebase from 'firebase'
-const firebaseConfig = {
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+firebase.initializeApp({
   apiKey: 'AIzaSyDyA4k8AnSyy7c_KgAhfDvuELFNpg9y_-s',
   authDomain: 'slack-clone-fe6cf.firebaseapp.com',
   projectId: 'slack-clone-fe6cf',
@@ -7,9 +10,8 @@ const firebaseConfig = {
   messagingSenderId: '418078400045',
   appId: '1:418078400045:web:486b847006bdcc0bafe2a4',
   measurementId: 'G-T0RLSH3MF0',
-}
-const firebaseApp = firebase.initializeApp(firebaseConfig)
-const db = firebaseApp.firestore()
+})
+const db = firebase.firestore()
 const auth = firebase.auth()
 const provider = new firebase.auth.GoogleAuthProvider()
 
