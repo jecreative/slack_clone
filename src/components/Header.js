f2d93877243e9b55221868a849c73fc4fef13d95
Jordan Esguerra <creative@jordanesguerra.com>
2020-12-11 21:05:39 -0800

    35) Search functionality complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,62 @@
+import { useState, useEffect, useRef } from 'react'
+import { useHistory } from 'react-router-dom'
 import { Avatar } from '@material-ui/core'
 import AccessTimeIcon from '@material-ui/icons/AccessTime'
 import SearchIcon from '@material-ui/icons/Search'
 import HelpOutlineIcon from '@material-ui/icons/HelpOutline'
 import ExitToAppIcon from '@material-ui/icons/ExitToApp'
 import Tooltip from '@material-ui/core/Tooltip'
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos'
 
 import { useStateValue } from '../context/StateProvider'
 import { actionTypes } from '../context/reducer'
+import db from '../firebase'
 
-const Header = ({ toggleSidebar, setToggleSidebar }) => {
+const Header = () => {
+  const [channels, setChannels] = useState([])
+  const [searchInput, setSearchInput] = useState('')
+  const [searchedChannels, setSearchedChannels] = useState([])
   const [{ user }, dispatch] = useStateValue()
+  const searchRef = useRef()
+
+  const history = useHistory()
+
+  useEffect(() => {
+    db.collection('rooms').onSnapshot((snapshot) => {
+      setChannels(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          name: doc.data().name,
+        }))
+      )
+    })
+  }, [])
+
+  const searchChannels = (e) => {
+    setSearchInput(e.target.value)
+    if (searchInput === '') setSearchedChannels([])
+    else
+      setSearchedChannels(
+        channels !== [] &&
+          channels.filter(
+            (channel) =>
+              channel.name
+                .toLowerCase()
+                .split(' ')
+                .join('')
+                .includes(searchInput.toLowerCase()) && channel.name
+          )
+      )
+    console.log(searchedChannels)
+  }
+
+  const visitChannel = (id) => {
+    if (id) {
+      history.push(`/room/${id}`)
+      setSearchedChannels([])
+      setSearchInput('')
+    }
+  }
 
   const logoutUser = () => {
     dispatch({ type: actionTypes.LOGOUT_USER })
@@ -25,8 +72,35 @@ const Header = ({ toggleSidebar, setToggleSidebar }) => {
         <AccessTimeIcon />
       </div>
       <div className='header_search'>
-        <SearchIcon />
-        <input type='search' placeholder='Search' />
+        <div className='header_searchInput'>
+          <SearchIcon />
+          <input
+            type='search'
+            ref={searchRef}
+            value={searchInput}
+            placeholder='Search'
+            onChange={searchChannels}
+          />
+        </div>
+        <div
+          className={
+            searchedChannels.length !== 0
+              ? 'search_results active'
+              : 'search_results'
+          }
+        >
+          {searchChannels.length !== 0 &&
+            searchedChannels.map((channel) => (
+              <div key={channel.id}>
+                <div className='searchedItem'>
+                  <h4>{channel.name}</h4>
+                  <ArrowForwardIosIcon
+                    onClick={() => visitChannel(channel.id)}
+                  />
+                </div>
+              </div>
+            ))}
+        </div>
       </div>
       <div className='header_right'>
         <HelpOutlineIcon />
